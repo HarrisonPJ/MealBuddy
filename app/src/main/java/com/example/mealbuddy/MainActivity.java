5c72f37534faeef209514e04f49947215ae31503
Akshay Vaswani <34140914+AkshayVaswani@users.noreply.github.com>
2020-02-05 19:59:21 -0500

    Testing Git Pushes

--- app/src/main/java/com/example/mealbuddy/MainActivity.java
+++ app/src/main/java/com/example/mealbuddy/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.mealbuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+Button butt;
+int counter;
+TextView textCounter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counter = 0;
+
+        butt=findViewById(R.id.butt);
+        textCounter = findViewById(R.id.textView);
+
+        butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                textCounter.setText(counter);
+            }
+        });
+
     }
 }
